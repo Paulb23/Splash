d27706cbeb0d33ff8fd72788a0efd537879ea7e4
Paul <crazy046@hotmail.co.uk>
2015-04-04 22:47:28 +0100

    vector 2 and vector 4

--- include/splash/Splash_vector.h
+++ include/splash/Splash_vector.h
@@ -29,6 +29,17 @@ extern "C" {
  ---------------------------------------------------------------------------*/
 
 /*!--------------------------------------------------------------------------
+  @brief    Splash_vector2
+
+  The Splash vector2 structure.
+\----------------------------------------------------------------------------*/
+typedef struct Splash_vector2 {
+  double x; /**< The x in the vector */
+  double y; /**< The y in the vector */
+} Splash_vector2;
+
+
+/*!--------------------------------------------------------------------------
   @brief    Splash_vector3
 
   The Splash vector3 structure.
@@ -40,19 +51,50 @@ typedef struct Splash_vector3 {
 } Splash_vector3;
 
 
+/*!--------------------------------------------------------------------------
+  @brief    Splash_vector4
+
+  The Splash vector4 structure.
+\----------------------------------------------------------------------------*/
+typedef struct Splash_vector4 {
+  double x; /**< The x in the vector */
+  double y; /**< The y in the vector */
+  double z; /**< The z in the vector */
+  double w; /**< The w in the vector */
+} Splash_vector4;
+
 /*---------------------------------------------------------------------------
                             Function prototypes
  ---------------------------------------------------------------------------*/
 
 /*!--------------------------------------------------------------------------
+  @brief    Creates a Splash_vector2
+  @return 	returns a Splash_vector2
+
+  Creates and returns a Splash_vector2
+
+\-----------------------------------------------------------------------------*/
+extern DLL_EXPORT Splash_vector2 SPLASHCALL splash_vector2_create(double x, double y);
+
+
+/*!--------------------------------------------------------------------------
   @brief    Creates a Splash_vector3
-  @return 	returns a Splash_vector3
+  @return   returns a Splash_vector3
 
   Creates and returns a Splash_vector3
 
 \-----------------------------------------------------------------------------*/
 extern DLL_EXPORT Splash_vector3 SPLASHCALL splash_vector3_create(double x, double y, double z);
 
+
+/*!--------------------------------------------------------------------------
+  @brief    Creates a Splash_vector4
+  @return   returns a Splash_vector4
+
+  Creates and returns a Splash_vector4
+
+\-----------------------------------------------------------------------------*/
+extern DLL_EXPORT Splash_vector4 SPLASHCALL splash_vector4_create(double x, double y, double z, double w);
 
 
 /* end C definitions */
--- src/data_structures/Splash_vector.c
+++ src/data_structures/Splash_vector.c
@@ -27,12 +27,36 @@
  ---------------------------------------------------------------------------*/
 
 /*!--------------------------------------------------------------------------
+  @brief    Creates a Splash_vector2
+  @return 	returns a Splash_vector2
+
+  Creates and returns a Splash_vector3
+
+\-----------------------------------------------------------------------------*/
+Splash_vector2 splash_vector2_create(double x, double y) {
+	return ( Splash_vector2 ) { x, y };
+}
+
+
+/*!--------------------------------------------------------------------------
   @brief    Creates a Splash_vector3
-  @return 	returns a Splash_vector3
+  @return   returns a Splash_vector3
 
   Creates and returns a Splash_vector3
 
 \-----------------------------------------------------------------------------*/
 Splash_vector3 splash_vector3_create(double x, double y, double z) {
-	return ( Splash_vector3 ) { x, y, z };
+  return ( Splash_vector3 ) { x, y, z };
+}
+
+
+/*!--------------------------------------------------------------------------
+  @brief    Creates a Splash_vector4
+  @return   returns a Splash_vector4
+
+  Creates and returns a Splash_vector4
+
+\-----------------------------------------------------------------------------*/
+Splash_vector4 splash_vector4_create(double x, double y, double z, double w) {
+  return ( Splash_vector4 ) { x, y, z, w };
 }
